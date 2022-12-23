709245a3f8352e0de1722c0750f7a187d6718a8c
vitalyvitmens <otm_@tut.by>
2022-12-24 00:37:25 +0300

    added file my_profile.js

---
+++ my_profile.js
@@ -0,0 +1,22 @@
+$(function () {
+  var p1 = $('#soft1')
+  var p2 = $('#soft2')
+
+  p1.animate({ opacity: 0.5 })
+  p1.animate({ left: '-=90%', left: '+=90%', fontSize: '-=50%' }, 1000)
+  p1.animate({ left: '+=90%', left: '-=90%', fontSize: '+=50%' }, 500)
+  p1.animate({ opacity: 1.0 }, 100)
+  p1.animate({ opacity: 0.1 }, 100)
+  p1.animate({ opacity: 1.0 }, 100)
+  p1.animate({ opacity: 0.1 }, 100)
+  p1.animate({ opacity: 1.0 }, 100)
+  p1.animate({ opacity: 0.1 }, 100)
+  p1.animate({ opacity: 1.0 }, 100)
+  p1.animate({ opacity: 0.1 }, 100)
+  p1.animate({ opacity: 1.0 }, 100)
+
+  p2.animate({ opacity: 0.5 })
+  p2.animate({ left: '-=90%', left: '+=90%', fontSize: '-=50%' }, 1000)
+  p2.animate({ left: '+=90%', left: '-=90%', fontSize: '+=50%' }, 1300)
+  p2.animate({ opacity: 1.0 })
+})
