fe64d6ca4da6a634ffa5f48d146b670410bb4a36
vitalyvitmens <otm_@tut.by>
2022-12-25 00:54:28 +0300

    added data in files index.html, my_profile.css, my_profile.js

--- my_profile.js
+++ my_profile.js
@@ -16,3 +16,15 @@ $(function () {
   p2.animate({ left: '+=90%', left: '-=90%', fontSize: '+=50%' }, 1300)
   p2.animate({ opacity: 1.0 })
 })
+window.onload = function () {
+  var els = document.querySelectorAll('#cite')
+  for (var i = 0; i < els.length; i++)
+    els[i].addEventListener(
+      'click',
+      function () {
+        if (this.getAttribute('data-href2'))
+          window.open(this.getAttribute('data-href2'))
+      },
+      false
+    )
+}
